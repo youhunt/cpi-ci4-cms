e8c96361fc1307a00a9fd750e4f2f7398f7ca890
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-11 17:20:26 +0700

    update redirect logout to root

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -75,8 +75,8 @@ class Auth extends ShieldAuth
      */
     public array $redirects = [
         'register'          => '/',
-        'login'             => '/',
-        'logout'            => 'login',
+        'login'             => '/admin',
+        'logout'            => '/',
         'force_reset'       => '/',
         'permission_denied' => '/',
         'group_denied'      => '/',
