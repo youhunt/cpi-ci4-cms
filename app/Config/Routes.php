2287a51dae22d14b24da290080be4532f6a20662
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-11 16:24:34 +0700

    Add admin CRUD routes for CMS modules

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,20 +12,46 @@ $routes->get('language/switch/(:alpha)', 'Language::switch/$1');
 
 service('auth')->routes($routes);
 
-// Public pages can be extended later with localized routes:
-// $routes->get('(:alpha)/products', 'Products::index/$1');
-// $routes->get('(:alpha)/products/(:segment)', 'Products::show/$1/$2');
-// $routes->get('(:alpha)/projects', 'Projects::index/$1');
-// $routes->get('(:alpha)/projects/(:segment)', 'Projects::show/$1/$2');
-// $routes->post('(:alpha)/inquiry', 'Inquiry::store/$1');
+$routes->post('inquiry', 'Inquiry::store');
+$routes->post('en/inquiry', 'Inquiry::store/en');
+$routes->post('id/inquiry', 'Inquiry::store/id');
 
 // Shield protected CMS routes.
-// After installing CodeIgniter Shield, the `session` filter protects admin pages.
 $routes->group('admin', ['filter' => 'session'], static function ($routes) {
     $routes->get('/', 'Admin\Dashboard::index');
+
     $routes->get('products', 'Admin\Products::index');
+    $routes->get('products/new', 'Admin\Products::new');
+    $routes->post('products', 'Admin\Products::create');
+    $routes->get('products/(:num)/edit', 'Admin\Products::edit/$1');
+    $routes->post('products/(:num)', 'Admin\Products::update/$1');
+    $routes->post('products/(:num)/delete', 'Admin\Products::delete/$1');
+
     $routes->get('projects', 'Admin\Projects::index');
+    $routes->get('projects/new', 'Admin\Projects::new');
+    $routes->post('projects', 'Admin\Projects::create');
+    $routes->get('projects/(:num)/edit', 'Admin\Projects::edit/$1');
+    $routes->post('projects/(:num)', 'Admin\Projects::update/$1');
+    $routes->post('projects/(:num)/delete', 'Admin\Projects::delete/$1');
+
     $routes->get('network', 'Admin\Network::index');
+    $routes->get('network/new', 'Admin\Network::new');
+    $routes->post('network', 'Admin\Network::create');
+    $routes->get('network/(:num)/edit', 'Admin\Network::edit/$1');
+    $routes->post('network/(:num)', 'Admin\Network::update/$1');
+    $routes->post('network/(:num)/delete', 'Admin\Network::delete/$1');
+
+    $routes->get('news', 'Admin\News::index');
+    $routes->get('news/new', 'Admin\News::new');
+    $routes->post('news', 'Admin\News::create');
+    $routes->get('news/(:num)/edit', 'Admin\News::edit/$1');
+    $routes->post('news/(:num)', 'Admin\News::update/$1');
+    $routes->post('news/(:num)/delete', 'Admin\News::delete/$1');
+
     $routes->get('inquiries', 'Admin\Inquiries::index');
+    $routes->get('inquiries/(:num)', 'Admin\Inquiries::show/$1');
+    $routes->post('inquiries/(:num)', 'Admin\Inquiries::update/$1');
+    $routes->post('inquiries/(:num)/delete', 'Admin\Inquiries::delete/$1');
+
     $routes->get('settings', 'Admin\Settings::index');
 });
